a86966abae26efd45698ffdb1e6aed4e6da9f0b1
Irislin114 <iris5968327@gmail.com>
2016-01-27 22:03:58 +0800

    homework

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe User do
+
+  # 160127 homewok
+
+  context "admin user" do
+    it"can create a admin user" do
+      user = create(:admin)
+      expect(user.role).to eq "admin"
+    end
+  end
 end
